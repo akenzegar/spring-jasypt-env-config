67905020b1bd06c0b301fe9c8ada1924b2ce0ac9
prashant <prashant.kumashi@wipro.com>
2019-10-10 13:08:00 +0530

    Initial project content commit

---
+++ src/main/java/com/samples/pck/springjasypt/SpringJasyptEnvConfigApplication.java
@@ -0,0 +1,13 @@
+package com.samples.pck.springjasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJasyptEnvConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJasyptEnvConfigApplication.class, args);
+	}
+
+}
